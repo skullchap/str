5f82e7a563a28ab9d9394bf2daebf18470147ed2
skullchap <11740883+skullchap@users.noreply.github.com>
2025-01-08 11:25:43 +0400

    Create str.h

---
+++ str.h
@@ -0,0 +1,23 @@
+/* started 01.07.2025 - skullchap - MIT */
+
+typedef struct str str;
+
+str*	newstrn(char* cstr, long n);
+str*	newstr(char* cstr);
+void	freestr(str *s);
+char*	cstr(str *s);
+long	strn(str *s);
+str*	strize(str *s, long n);		/* str resize */
+
+void	strallocfn(void*(*)(unsigned long));
+void	strdeallocfn(void(*)(void*));
+void	strreallocfn(void*(*)(void*, unsigned long));
+
+#define STR(S) newstrn((S), sizeof(S)-1)
+
+/* todo: add all string.h funcs below */
+str*	sstrcat(str *l, str *r);
+int	sstrcmp(str *l, str *r);
+
+/* todo: add anything utf8 related below */
+long	runen(str *s);
