0d65f3306606d9a06dba99edba1f9b3d5853dcd4
skullchap <11740883+skullchap@users.noreply.github.com>
2025-01-12 00:39:43 +0400

    str.c: cache runen, shorter names, scatn

--- str.c
+++ str.c
@@ -7,19 +7,18 @@ typedef unsigned long ulong;
 typedef struct FILE FILE;
 enum	{SEEK_END=	2};
 
-FILE*	fopen(const char*, const char*);
+FILE*	fopen(char*, char*);
 ulong	fread(void*, ulong, ulong, FILE*);
 int	fseek(FILE*, long, int);
 long	ftell(FILE*);
 void	rewind(FILE*);
 int	fclose(FILE*);
-
 void*	malloc(ulong);
 void	free(void*);
 void*	realloc(void*, ulong);
-void*	memcpy(void*, const void*, ulong);
-ulong	strlen(const char *);
-int	strcmp(const char*, const char*);;
+void*	memcpy(void*, void*, ulong);
+ulong	strlen(char *);
+int	strcmp(char*, char*);
 
 void*(*allocf)(ulong) = malloc;
 void(*deallocf)(void*) = free;
@@ -27,7 +26,7 @@ void*(*realocf)(void*, ulong) = realloc;
 
 struct str
 {
-	long	n;
+	long n, runen;
 };
 
 #define cast(T,V)	((T)(V))
@@ -44,6 +43,7 @@ allocstr(long n)		/* not reusing reallocstr, cause custom global realocfn can ac
 	if(!s)
 		return 0;
 	s->n=n;
+	s->runen=0;
 	term(s);
 	return (str*)STREND(s);
 }
@@ -57,6 +57,7 @@ reallocstr(str *s, long n)
 		return 0;
 	s=p;
 	s->n=n;
+	s->runen=0;
 	term(s);
 	return (str*)STREND(s);
 }
@@ -80,8 +81,8 @@ void	strallocfn(void *(*fn)(unsigned long))		{allocf=fn;}
 void	strdeallocfn(void (*fn)(void*))			{deallocf=fn;}
 void	strreallocfn(void *(*fn)(void *, ulong))	{realocf = fn;}
 
-str*	strcopy(str *s)		{return newstrn(cstr(s), strn(s));}
-str*	strize(str *s, long n)	{return reallocstr(s, n);}
+str*	scopy(str *s)		{return newstrn(cstr(s), strn(s));}
+str*	sresize(str *s, long n)	{return reallocstr(s, n);}
 
 str*
 sreadfile(char *fpath)
@@ -114,36 +115,65 @@ cleanup:
 }
 
 str*
-sstrcat(str *l, str *r)
+scat(str *l, str *r)
 {
-	str *s = allocstr(strn(l) + strn(r));
+/* 	str *s = allocstr(strn(l) + strn(r));
 	if(!s)
 		return 0;
 	memcpy(s, l, strn(l));
-	memcpy(cast(uchar*, s) + strn(l), r, strn(r));
+	memcpy(cast(uchar*, s)+ strn(l), r, strn(r));
+	return s; */
+
+	str *sa[2];
+	sa[0]= l, sa[1]= r;
+	return scatn(sa, 2);
+}
+
+str*
+scatn(str *sa[], int n)
+{
+        str *s, sp;
+	int i; 
+	long slen;
+
+	for(i=0,slen=0; i<n; ++i)
+		slen += strn(sa[i]); 
+
+	s = allocstr(slen);
+	if(!s)
+		return 0;
+
+	for(i=0,slen=0; i<n; ++i){
+		memcpy(cast(uchar*, s)+ slen, sa[i], strn(sa[i]));
+		slen += strn(sa[i]);
+	}
 	return s;
 }
 
-int	sstrcmp(str *l, str *r)	{return strcmp((char*)l, (char*)r);}
+int	scmp(str *l, str *r)	{return strcmp((char*)l, (char*)r);}
 
 long
 runen(str *s)
 {
-	long runen = 0;
-	uchar *ptr = (uchar *)s;
+	long runen = cast(str*, STRSTART(s))->runen;
+	uchar *p = (uchar *)s;
 
-	while(*ptr != '\0'){
-		uchar b = *ptr;
+	if(runen > 0)
+		return runen;
+
+	while(*p != '\0'){
+		uchar b = *p;
 		if((b & 0x80) == 0){
-			ptr += 1;
+			p += 1;
 		}else if((b & 0xE0) == 0xC0){
-			ptr += 2;
+			p += 2;
 		}else if((b & 0xF0) == 0xE0){
-			ptr += 3;
+			p += 3;
 		}else if((b & 0xF8) == 0xF0){
-			ptr += 4;
+			p += 4;
 		}
 		runen++;
 	}
+	cast(str*, STRSTART(s))->runen = runen;
 	return runen;
 }
